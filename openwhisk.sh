73d3d13befdc548303fb11fa101b557c4ce0371b
Maxim Generalov <57221462+maSkon1@users.noreply.github.com>
2023-01-12 16:11:43 +0300

    Add files via upload

--- openwhisk.sh
+++ openwhisk.sh
@@ -19,13 +19,13 @@ export OW_DB=CouchDB
 export OW_DB_PROTOCOL=http
 export OW_DB_HOST=10.20.14.190
 export OW_DB_PORT=5984
-export OW_DB_USERNAME=admin1
+export OW_DB_USERNAME=admin
 export OW_DB_PASSWORD=cZdg3qbSf
 export ENVIRONMENT=local
 echo ENVIRONMENT=local >>  /etc/bash.bashrc
 ansible-playbook -i environments/$ENVIRONMENT setup.yml
 ansible-playbook -i environments/$ENVIRONMENT prereq.yml
-cd /home/test/openwhisk/
+cd /home/mgen1/openwhisk/
 ./gradlew distDocker
 cd ansible
 ansible-playbook -i environments/$ENVIRONMENT couchdb.yml
@@ -35,6 +35,6 @@ ansible-playbook -i environments/$ENVIRONMENT openwhisk.yml
 ansible-playbook -i environments/$ENVIRONMENT postdeploy.yml
 ansible-playbook -i environments/$ENVIRONMENT apigateway.yml
 ansible-playbook -i environments/$ENVIRONMENT routemgmt.yml
-cd /home/test/openwhisk/bin
+cd /home/mgen1/openwhisk/bin
 export PATH=$PATH:$PWD
-echo PATH=$PATH:/home/test/openwhisk/bin >>  /etc/bash.bashrc
+echo PATH=$PATH:/home/mgen1/openwhisk/bin >>  /etc/bash.bashrc
